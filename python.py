0f73cc152d2c7652dc050fe55af7ee519c286422
Chelsey Kallman <2010144@student.goddardusd.com>
2023-12-13 22:35:55 -0600

    testing 1 link

--- python.py
+++ python.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods = {'GET', 'POST'})
+@app.route('/')
 def home():
-    if request.method == "GET":
-        return render_template('index.html')
+    return render_template('index.html')
+
+@app.route('/classlist')
+def classList():
+    return render_template('classlist.html')
 
 if __name__ == '__main__':
     app.run(debug = True)
